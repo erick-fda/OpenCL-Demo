79dcbf1ee4be7a43c5c32af515bdb3aa0bf2c90f
Erick FdA <erick.fernandez.da@gmail.com>
2017-10-28 18:45:09 -0700

    Reading platforms with OpenCL

--- COMP8904_Asg02/Part02/Part02.cpp
+++ COMP8904_Asg02/Part02/Part02.cpp
@@ -13,23 +13,19 @@
 /*========================================================================================
 	Dependencies
 ========================================================================================*/
-#include <chrono>
-#include <ctime>
 #include <iostream>
-#include <random>
-//#include "Pixel.h"
+#include <map>
+#include <vector>
 
-#include <stdio.h>
 #include <CL/cl.h>
 
 /*========================================================================================
 	Fields
 ========================================================================================*/
-const int NUM_PIXELS = 1000000;
-//std::vector<float4> _pixels;
-int _startTime;
-int _endTime;
-int _timeToRun;
+cl_uint numPlatforms;
+std::vector<cl_platform_id> platformIds;
+std::map<cl_platform_id, std::vector<cl_device_id>> platforms;
+cl_platform_id cpuToUse;
 
 /*========================================================================================
 	Main Function
@@ -40,14 +36,83 @@ int _timeToRun;
 */
 int main()
 {
-	cl_int err;
-	cl_uint numPlatforms;
+	int result = 0;
 
-	err = clGetPlatformIDs(0, NULL, &numPlatforms);
-	if (CL_SUCCESS == err)
-		printf("\nDetected OpenCL platforms: %d", numPlatforms);
-	else
-		printf("\nError calling clGetPlatformIDs. Error code: %d", err);
+	/* Get the number of available platforms. */
+	numPlatforms = 0;
+	clGetPlatformIDs(0, nullptr, &numPlatforms);
+
+	/* Get the IDs of available platforms. */
+	platformIds = std::vector<cl_platform_id>(numPlatforms);
+	result = clGetPlatformIDs(numPlatforms, platformIds.data(), nullptr);
+
+	/* Get the devices for each platform. */
+	platforms = std::map<cl_platform_id, std::vector<cl_device_id>>();
+	for each (cl_platform_id eachPlatform in platformIds)
+	{
+		cl_uint numDevices = 0;
+		clGetDeviceIDs(eachPlatform, CL_DEVICE_TYPE_ALL, 0, nullptr, &numDevices);
+
+		platforms[eachPlatform] = std::vector<cl_device_id>(numDevices);
+		clGetDeviceIDs(eachPlatform, CL_DEVICE_TYPE_ALL, numDevices, platforms[eachPlatform].data(), nullptr);
+	}
+
+	/* Print out platforms and devices. */
+	std::cout << "Detected the following platforms and devices:\n";
+	for each (std::pair<cl_platform_id, std::vector<cl_device_id>> eachPlatform in platforms)
+	{
+		std::cout << "\t" << eachPlatform.first << "\n";
+
+		for each (cl_device_id eachDevice in eachPlatform.second)
+		{
+			cl_device_type deviceType;
+			clGetDeviceInfo(eachDevice, CL_DEVICE_TYPE, sizeof(cl_device_id), &deviceType, nullptr);
+
+			std::string deviceTypeString;
+			switch (deviceType)
+			{
+			case CL_DEVICE_TYPE_CPU:
+				deviceTypeString = "CPU";
+				break;
+			case CL_DEVICE_TYPE_GPU:
+				deviceTypeString = "GPU";
+				break;
+			default:
+				deviceTypeString = "OTHER";
+				break;
+			}
+
+			std::cout << "\t\t" << eachDevice << " (" << deviceTypeString.data() << ")\n";
+		}
+	}
+	std::cout << "\n";
+
+	/* Identify which CPU to use. */
+	for each (std::pair<cl_platform_id, std::vector<cl_device_id>> eachPlatform in platforms)
+	{
+		bool isCpu = true;
+
+		for each (cl_device_id eachDevice in eachPlatform.second)
+		{
+			cl_device_type deviceType;
+			clGetDeviceInfo(eachDevice, CL_DEVICE_TYPE, sizeof(cl_device_id), &deviceType, nullptr);
+
+			if (deviceType == CL_DEVICE_TYPE_GPU)
+			{
+				isCpu = false;
+				break;
+			}
+		}
+
+		if (isCpu)
+		{
+			cpuToUse = eachPlatform.first;
+			break;
+		}
+	}
+
+	/* Create open  */
 
 	std::cin.ignore();
+	return 0;
 }
